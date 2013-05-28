adcdd48aec22dc76ea72a3b8d7b4c08588adbeed
stephen.xzien <stephen@xzien.com>
2013-05-28 20:07:53 +0100

    Initial Commit

---
+++ Hour Rate/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hour Rate
+//
+//  Created by Stephen Kerr on 28/05/2013.
+//  Copyright (c) 2013 Xzien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
